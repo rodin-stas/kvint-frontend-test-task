2cbc89352ef9e6d62b52d5a19dbe3f33ccb5ac59
Dmitriy Bogatkin <bogatkin@tortstudio.com>
2021-03-26 11:29:35 +0300

    storybook base project

---
+++ src/components/TestComponent/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ComponentWrapper } from './index.styled';
+
+const TestComponent: React.FC = () => {
+  return <ComponentWrapper>Place to do some things</ComponentWrapper>;
+};
+
+export default TestComponent;

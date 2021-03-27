7d20a144a747fe1931a00ca6a176820937745c57
Rodin Stasnislav <rodinstas@gmail.com>
2021-03-27 18:52:27 +0300

    add SwitchC

--- src/components/TestComponent/index.styled.tsx
+++ src/components/TestComponent/index.styled.tsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
 
 // eslint-disable-next-line import/prefer-default-export
-export const ComponentWrapper = styled.div``;
+export const ComponentWrapper = styled.div`
+  text-align: center;
+`;
